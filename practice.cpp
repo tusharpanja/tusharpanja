491bf2851332a91bc64308af75da1aed69a9bb90
TUSHAR PANJA <mailtusharpanja@gmail.com>
2024-01-14 09:28:08 +0530

    Update practice.cpp
    Lol

--- practice.cpp
+++ practice.cpp
@@ -1,30 +1,35 @@
-#include <iostream>
-using namespace std;
+import java.util.Scanner;
 
-int main()
-{
-    int n, i;
-    float num[100], sum=0.0, average;
+public class AnnualIncomeCalculator {
 
-    cout << "Enter the numbers of data: ";
-    cin >> n;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    while (n > 100 || n <= 0)
-    {
-        cout << "Error! number should in range of (1 to 100)." << endl;
-        cout << "Enter the number again: ";
-        cin >> n;
+        System.out.print("Enter the number of employees: ");
+        int numEmployees = scanner.nextInt();
+
+        for (int i = 1; i <= numEmployees; i++) {
+            System.out.print("Enter the number of years of experience for Employee " + i + ": ");
+            int yearsOfExperience = scanner.nextInt();
+
+            if (yearsOfExperience > 0 && yearsOfExperience < 15) {
+                System.out.print("Enter the shift allowance percentage for Employee " + i + ": ");
+                double shiftAllowancePercentage = scanner.nextDouble();
+
+                if (shiftAllowancePercentage > 0 && shiftAllowancePercentage < 20) {
+                    double dayShiftSalary = 12000 + (2000 * yearsOfExperience);
+                    double nightShiftSalary = dayShiftSalary * (1 + shiftAllowancePercentage / 100);
+                    double annualIncome = (dayShiftSalary * 6 + nightShiftSalary * 6);
+
+                    System.out.println("Annual income of Employee " + i + ": Rs." + annualIncome);
+                } else {
+                    System.out.println("<> is an invalid shift allowance percentage for Employee " + i);
+                }
+            } else {
+                System.out.println("<> is an invalid years of experience for Employee " + i);
+            }
+        }
+
+        scanner.close();
     }
-
-    for(i = 0; i < n; ++i)
-    {
-        cout << i + 1 << ". Enter number: ";
-        cin >> num[i];
-        sum += num[i];
-    }
-
-    average = sum / n;
-    cout << "Average = " << average;
-
-    return 0;
 }
